f38ec377ab41789fe25cdc07030955851c49f086
Fabian Bienk <fb@hexdmp.com>
2026-06-05 09:19:01 +0200

    fix: move theme watcher to module scope

--- src/composables/useTheme.ts
+++ src/composables/useTheme.ts
@@ -2,14 +2,13 @@ import { ref, watchEffect } from 'vue'
 
 const theme = ref<'dark' | 'light'>('dark')
 
+watchEffect(() => {
+  document.documentElement.setAttribute('data-theme', theme.value)
+})
+
 export function useTheme() {
   function toggleTheme() {
     theme.value = theme.value === 'dark' ? 'light' : 'dark'
   }
-
-  watchEffect(() => {
-    document.documentElement.setAttribute('data-theme', theme.value)
-  })
-
   return { theme, toggleTheme }
 }
